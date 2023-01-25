c813aaf1a730975e5e80d4188f47557c19fcc422
Wesley Diorrani Ferreira <wesley_d_ferreira@estudante.sesisenai.org.br>
2023-01-25 14:16:18 -0300

    Update atravessarRua.java

--- atravessarRua.java
+++ atravessarRua.java
@@ -1,1 +1,37 @@
 "# atravessarRua" 
+
+  package procedimentos;
+import java.util.Scanner;
+public class procedimentos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        
+		Scanner entrada = new Scanner (System.in);
+		
+		int x,y,d;
+		
+		System.out.println("Qual será o ponto inicial da sua partida?");
+		x = entrada.nextInt();
+		
+		System.out.println("A distancia que cada passo percorreu?");
+		d = entrada.nextInt();
+		
+		System.out.println("Digite a distancia a ser percorrida ");
+		y = entrada.nextInt();
+		
+		atravessando(x,y,d);
+	}
+	
+	public static void atravessando(int x, int y, int d) {
+		int contadorAndar = 0;
+		while(x < y) {
+			contadorAndar++;
+			x += d;
+		}
+		if(x >= y) {
+			System.out.println("A quantidade de passos dados foi de "+contadorAndar);
+		}
+	}
+
+}
